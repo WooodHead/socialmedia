9d555426f17e544616c59cf89f07077230e3b0ca
Sorin Davidoi <sorin.davidoi@gmail.com>
2015-11-26 09:35:49 +0100

    Implement routing

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var index = require('./routes/index')();
 var items = require('./routes/items')();
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -27,6 +28,7 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/api/items', items);
 
 // catch 404 and forward to error handler
