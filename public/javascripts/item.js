e03d6f0ba161f260bcdd30ff85662a5439b21d03
Sorin Davidoi <sorin.davidoi@gmail.com>
2015-11-26 18:11:11 +0100

    Implement scheduler

--- public/javascripts/item.js
+++ public/javascripts/item.js
@@ -9,7 +9,10 @@ app.directive('item', ['ItemFactory', 'ItemsSocket',  function(ItemFactory, Item
     controller: function($element, $attrs) {
     },
     link: function(scope, element, attrs) {
-      scope.item = ItemFactory.get({ id: attrs.id });
+      scope.item = ItemFactory.get({ id: attrs.id }, function(res) {
+        // It is a string by default
+        scope.item.scheduled = new Date(scope.item.scheduled);
+      });
 
       ItemsSocket.on('items:' + attrs.id + ':put', function(item) {
         scope.item = item;
